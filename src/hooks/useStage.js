061dcd7b8e994c84c4b76cf267098d6ef72d2505
AJ Kumar <ajanthan29894@gmail.com>
2019-09-21 16:56:52 +0100

    implemented row clear when filled

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -3,8 +3,31 @@ import { createStage } from '../gameHelpers';
 
 export const useStage = (player, resetPlayer) => {
   const [stage, setStage] = useState(createStage());
+  const [rowsCleared, setRowsCleared] = useState(0);
 
   useEffect(() => {
+    setRowsCleared(0);
+
+    //reduce applies function to each element in an array to reduce it to a single value
+    const sweepRows = newStage =>
+      newStage.reduce((acc, row) => {
+        //findIndex return the index of the first element to satisfy the condition. else it returns -1
+        if (row.findIndex(cell => cell[0] === 0) === -1) {
+          setRowsCleared(prev => prev + 1);
+          acc.unshift(new Array(newStage[0].length).fill([0, 'clear']));
+          return acc;
+          //row gets cleared here
+        }
+
+        //acc is the accumulator for the next stage. It checks if the row has any clear cells
+        //if it does, then push that row to the acc. if it doesnt then add an empty row to the top of the stage
+        //a row with no 0s will no be pushed into acc.
+        //return acc as next stage
+
+        acc.push(row);
+        return acc;
+      }, []);
+
     const updateStage = prevStage => {
       //First flush stage
       const newStage = prevStage.map(row =>
@@ -27,11 +50,12 @@ export const useStage = (player, resetPlayer) => {
       // Then check if we have collided
       if (player.collided) {
         resetPlayer();
+        return sweepRows(newStage);
       }
       return newStage;
     };
     setStage(prev => updateStage(prev));
   }, [player, resetPlayer]);
 
-  return [stage, setStage];
+  return [stage, setStage, rowsCleared];
 };
